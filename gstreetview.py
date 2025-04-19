db5c226f57661a03ea4274cd1a95c96c10a8d262
finkegabriel <finkegabriel@gmail.com>
2025-04-19 16:10:27 -0700

    init

---
+++ gstreetview.py
@@ -0,0 +1,16 @@
+import google_streetview.api
+
+# Define parameters for street view api
+params = [{
+	'size': '600x300', # max 640x640 pixels
+	'location': '46.414382,10.013988',
+	'heading': '151.78',
+	'pitch': '-0.76',
+	'key': 'your_dev_key'
+}]
+
+# Create a results object
+results = google_streetview.api.results(params)
+
+# Download images to directory 'downloads'
+results.download_links('downloads')
